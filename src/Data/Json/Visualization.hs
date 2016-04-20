7971087e01cfec182133fd32fec81e5553d829f9
Harold Carr <harold.carr@gmail.com>
2016-04-19 18:08:20 -0700

    visualization starting to work (but no labels)

--- src/Data/Json/Visualization.hs
+++ src/Data/Json/Visualization.hs
@@ -6,7 +6,9 @@ import qualified Data.Aeson           as A (Value (String))
 import qualified Data.Graph.Inductive as G
 import qualified Data.GraphViz        as GV
 import           Data.Json.Validation
+import qualified Data.List            as L
 import qualified Data.Map.Strict      as M
+import qualified Data.Maybe           as MB
 import qualified Data.Text            as T
 import           Prelude              as P
 
@@ -14,19 +16,27 @@ toMap :: PathFieldValue -> M.Map T.Text [T.Text]
 toMap = foldr go M.empty
   where
     go :: ([T.Text], (T.Text, A.Value)) -> M.Map T.Text [T.Text] -> M.Map T.Text [T.Text]
-    go (path,(_,A.String value)) =
+    go (path,(_,A.String value)) acc =
         M.insertWith (++)
-                     (T.intercalate "/" (P.take 2 path))
-                     [T.drop 2 value]
+                     (T.intercalate "/" (P.take 2 path))  -- only use first part of path
+                     [T.drop 2 value]                     -- do not include "#/"
+                     acc
+    go  _ _ = error "NO"
 
--- mapToGraph :: (G.Graph gr) => M.Map T.Text [T.Text] -> gr T.Text T.Text
-mapToGraph :: M.Map T.Text [T.Text] -> G.Gr T.Text T.Text
-mapToGraph m = go (M.toList m) 0 [] []
+mkNodeMap :: M.Map T.Text [T.Text] -> M.Map T.Text (G.LNode T.Text)
+mkNodeMap m = snd $ foldr go (0::Int, M.empty) (L.nub $ M.keys m ++ P.concat (M.elems m))
   where
-    go          []  _ nodes0 edges0 = G.mkGraph nodes0 edges0
-    go ((k,vs):kvs) n nodes0 edges0 = let (n',moreNodes) = foldr go' (n,[]) vs
-                                      in go kvs n' (nodes0 ++ moreNodes) edges0
-    go' v (n,acc)                   = (n+1, (n, v) : acc)
+    go x (n,acc) = (n+1, M.insert x (n,x) acc)
+
+mapToGraph :: M.Map T.Text [T.Text] -> G.Gr T.Text ()
+mapToGraph m =
+    let nodeMap     = mkNodeMap m
+        edges       = M.foldrWithKey go [] m
+        go k vs acc = foldr (go' k) acc vs
+        go' k v acc = ( fst $ MB.fromJust $ M.lookup k nodeMap
+                      , fst $ MB.fromJust $ M.lookup v nodeMap
+                      , ()) : acc
+    in G.mkGraph (M.elems nodeMap) edges
 
 toDot :: PathFieldValue -> GV.DotGraph G.Node
 toDot = GV.graphToDot GV.nonClusteredParams . mapToGraph . toMap
@@ -36,6 +46,9 @@ import Data.Text
 :set -XOverloadedStrings
 (Just v) <- readJson "test/refs-simple-invalid.json"
 let pfvs = findInJson "$ref" v
-toMap pfvs
-toDot pfvs
+let m = toMap pfvs
+mkNodeMap m
+let dg = (mapToGraph . toMap) pfvs
+let td = Data.Json.Visualization.toDot pfvs
+runGraphviz td Png "/tmp/xx"
 -}
